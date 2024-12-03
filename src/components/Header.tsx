9c73299445922bbe2fd21353241630dcb312feea
Fikret Erkara <fikreterkara_26@hotmail.com>
2024-12-03 11:23:16 +0300

    created mobile sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,40 @@
+"use client"
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ThemeToggler from "./ThemeToggler";
+import MobileSidebar from "./MobileSidebar";
 
 const Header = () => {
+  const [show, handleShow] = useState(false);
+
+  const transitionHeader = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionHeader);
+    return () => window.removeEventListener("scroll", transitionHeader);
+  }, []);
+
   return (
-    <header className="flex items-center justify-between p-4 border-b-2 mb-8 sticky top-0 z-10 px-8 py-8 lg:px-20">
-      <h1 className="hidden lg:block text-3xl font-bold ">
-        <Link href="/">LevelUp DB</Link>
+    <header
+      className={`flex items-center justify-between p-4 border-b-2 border-gray-200 dark:border-white/5 mb-8 sticky top-0 z-10 px-8 py-8 lg:px-20 ${
+        show &&
+        "bg-gray-100 dark:bg-black-1 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-90 shadow-2xl"
+      }`}
+    >
+      <h1 className="hidden lg:block text-3xl font-bold">
+        <Link href="/">The Game DB</Link>
       </h1>
 
-      <div className="flex lg:hidden">{/* mobile menu */}</div>
+      <div className="flex lg:hidden">
+        <MobileSidebar />
+      </div>
+
       <ThemeToggler />
     </header>
   );
